4ee2ed1c48b6ddfccbfbdca957fd43b7b26c7c80
Hunter LaFaille <hlafaille@proton.me>
2024-10-08 14:53:39 +0000

    Switching to kindling + espresso

---
+++ src/java/com/kerosenelabs/espressoregistryapi/Main.java
@@ -0,0 +1,9 @@
+package com.kerosenelabs.espressoregistryapi;
+
+import java.lang.System;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Espresso!");
+    }
+}
